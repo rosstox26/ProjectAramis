b2ff395964acc185c46c4a0cb78f026d85293ffb
Sanjeev Kumar <clarifyr@gmail.com>
2016-11-15 11:55:10 -0500

    Testing

--- app/src/main/java/com/rosstox26/aramis/projectaramis/MainActivity.java
+++ app/src/main/java/com/rosstox26/aramis/projectaramis/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
+//Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
