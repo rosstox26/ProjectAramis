41615a25b23d92c156b9580c4395c385f9077eab
clarifyr <clarifyr@gmail.com>
2016-11-15 12:05:28 -0500

    test

--- app/src/main/java/com/rosstox26/aramis/projectaramis/MainActivity.java
+++ app/src/main/java/com/rosstox26/aramis/projectaramis/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-//Test
+//Test commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
